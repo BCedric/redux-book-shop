1c09ae720630dc9d5e1c4a779ba692e281ee96fc
BCedric <skalimero@gmx.fr>
2017-02-07 12:20:06 +0100

    use combineReducer

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const { createStore } = require('redux')
+const { combineReducers, createStore } = require('redux')
 const { Set } = require('immutable')
 
-const store = createStore((state, action) => {
+
+
+const bookReducer = (state, action) => {
   if (state === undefined) {
     state = { books: new Set() }
   }
-
   if (action.type === 'ADD_BOOK') {
     return { books: state.books.add(action.title)}
   }
-
   if (action.type === 'REMOVE_BOOK') {
     return { books: state.books.delete(action.title)}
   }
-
   return state
-})
+}
+
+const rootReducer = combineReducers({
+    bookReducer
+});
+
+const store = createStore(rootReducer)
 
 store.subscribe(() => {
   console.log('new state', store.getState())
