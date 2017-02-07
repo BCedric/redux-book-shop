11eeac1b07e3b1ffdbb52ccc54af8bd67ef19aa4
BCedric <skalimero@gmx.fr>
2017-02-07 15:28:31 +0100

    add custommers management

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const { combineReducers, createStore } = require('redux')
 const { Set } = require('immutable')
+const assert = require('assert');
 
 
 
 const bookReducer = (state, action) => {
   if (state === undefined) {
-    state = { books: new Set() }
+    state = { books: new Set(), customers: new Set()}
   }
   if (action.type === 'ADD_BOOK') {
-    return { books: state.books.add(action.title)}
+    return { books: state.books.add(action.title), customers: state.customers}
   }
   if (action.type === 'REMOVE_BOOK') {
-    return { books: state.books.delete(action.title)}
+    return { books: state.books.delete(action.title), customers: state.customers}
+  }
+  if (action.type === 'ADD_CUSTOMER') {
+    return { books: state.books, customers: state.customers.add(action.customer)}
+  }
+  if (action.type === 'REMOVE_CUSTOMER') {
+    return { books: state.books, customers: state.customers.delete(action.customer)}
   }
   return state
 }
 
-const rootReducer = combineReducers({
-    bookReducer
-});
 
-const store = createStore(rootReducer)
+const store = createStore(bookReducer)
 
 store.subscribe(() => {
   console.log('new state', store.getState())
@@ -28,5 +32,10 @@ store.subscribe(() => {
 
 
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
+assert.deepEqual({ books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
+assert.deepEqual({ books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
 store.dispatch({ type: 'REMOVE_BOOK', title: 'Война и мир, Voïna i mir' })
+assert.deepEqual( { books: new Set(), customers: new Set()}, store.getState())
+store.dispatch({ type: 'ADD_CUSTOMER', customer: 'Tintin' })
+assert.deepEqual({ books: new Set(), customers: new Set().add('Tintin')}, store.getState())
