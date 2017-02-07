f0bd904cb4765e93010dbd43e1a0126a38619ecb
BCedric <skalimero@gmx.fr>
2017-02-07 16:26:30 +0100

    add custommers management-1

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const { combineReducers, createStore } = require('redux')
+const { createStore } = require('redux')
 const { Set } = require('immutable')
-const assert = require('assert');
-
-
+const assert = require('assert')
 
 const bookReducer = (state, action) => {
   if (state === undefined) {
-    state = { books: new Set(), customers: new Set()}
+    state = {books: new Set(), customers: new Set()}
   }
   if (action.type === 'ADD_BOOK') {
-    return { books: state.books.add(action.title), customers: state.customers}
+    return {books: state.books.add(action.title), customers: state.customers}
   }
   if (action.type === 'REMOVE_BOOK') {
-    return { books: state.books.delete(action.title), customers: state.customers}
+    return {books: state.books.delete(action.title), customers: state.customers}
   }
   if (action.type === 'ADD_CUSTOMER') {
-    return { books: state.books, customers: state.customers.add(action.customer)}
+    return {books: state.books, customers: state.customers.add(action.customer)}
   }
   if (action.type === 'REMOVE_CUSTOMER') {
-    return { books: state.books, customers: state.customers.delete(action.customer)}
+    return {books: state.books, customers: state.customers.delete(action.customer)}
   }
   return state
 }
-
 
 const store = createStore(bookReducer)
 
@@ -30,12 +27,17 @@ store.subscribe(() => {
   console.log('new state', store.getState())
 })
 
+store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
+assert.deepStrictEqual({books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
 
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
-assert.deepEqual({ books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
-store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
-assert.deepEqual({ books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
+assert.deepStrictEqual({books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
+
 store.dispatch({ type: 'REMOVE_BOOK', title: 'Война и мир, Voïna i mir' })
-assert.deepEqual( { books: new Set(), customers: new Set()}, store.getState())
+assert.deepStrictEqual({books: new Set(), customers: new Set()}, store.getState())
+
 store.dispatch({ type: 'ADD_CUSTOMER', customer: 'Tintin' })
-assert.deepEqual({ books: new Set(), customers: new Set().add('Tintin')}, store.getState())
+assert.deepStrictEqual({books: new Set(), customers: new Set().add('Tintin')}, store.getState())
+
+store.dispatch({ type: 'REMOVE_CUSTOMER', customer: 'Tintin' })
+assert.deepStrictEqual({books: new Set(), customers: new Set()}, store.getState())
