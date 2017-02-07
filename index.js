682fc4b68ac885bb078088006be3206cb1816a9e
BCedric <skalimero@gmx.fr>
2017-02-07 17:10:13 +0100

    custommers management-2

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const { createStore } = require('redux')
-const { Set } = require('immutable')
 const assert = require('assert')
+const Immutable = require('immutable')
+
+const { createStore } = require('redux')
+const { Map, Set } = require('immutable')
 
 const bookReducer = (state, action) => {
   if (state === undefined) {
-    state = {books: new Set(), customers: new Set()}
+    state = Map({books: Set(), customers: Set()})
   }
   if (action.type === 'ADD_BOOK') {
-    return {books: state.books.add(action.title), customers: state.customers}
+    return Map({books: state.get('books').add(action.title), customers: state.get('customers')})
   }
   if (action.type === 'REMOVE_BOOK') {
-    return {books: state.books.delete(action.title), customers: state.customers}
+    return Map({books: state.get('books').delete(action.title), customers: state.get('customers')})
   }
   if (action.type === 'ADD_CUSTOMER') {
-    return {books: state.books, customers: state.customers.add(action.customer)}
+    return Map({books: state.get('books'), customers: state.get('customers').add(action.customer)})
   }
   if (action.type === 'REMOVE_CUSTOMER') {
-    return {books: state.books, customers: state.customers.delete(action.customer)}
+    return Map({books: state.get('books'), customers: state.get('customers').delete(action.customer)})
   }
   return state
 }
@@ -28,16 +30,16 @@ store.subscribe(() => {
 })
 
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
-assert.deepStrictEqual({books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
+assert(Immutable.is(Map({books: Set().add('Война и мир, Voïna i mir'), customers: Set()}), store.getState()) === true)
 
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
-assert.deepStrictEqual({books: new Set().add('Война и мир, Voïna i mir'), customers: new Set()}, store.getState())
+assert(Immutable.is(Map({books: Set().add('Война и мир, Voïna i mir'), customers: Set()}), store.getState()) === true)
 
 store.dispatch({ type: 'REMOVE_BOOK', title: 'Война и мир, Voïna i mir' })
-assert.deepStrictEqual({books: new Set(), customers: new Set()}, store.getState())
+assert(Immutable.is(Map({books: Set(), customers: Set()}), store.getState()) === true)
 
 store.dispatch({ type: 'ADD_CUSTOMER', customer: 'Tintin' })
-assert.deepStrictEqual({books: new Set(), customers: new Set().add('Tintin')}, store.getState())
+assert(Immutable.is(Map({books: Set(), customers: Set().add('Tintin')}), store.getState()) === true)
 
 store.dispatch({ type: 'REMOVE_CUSTOMER', customer: 'Tintin' })
-assert.deepStrictEqual({books: new Set(), customers: new Set()}, store.getState())
+assert(Immutable.is(Map({books: Set(), customers: Set()}), store.getState()) === true)
