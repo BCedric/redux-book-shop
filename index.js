a10c953b6be602731cd039ef5100ac3f38a8af6a
BCedric <skalimero@gmx.fr>
2017-02-08 14:12:34 +0100

    chore: use combineReducer (#7)
    Fixes #3

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const assert = require('assert')
-const { createStore } = require('redux')
-const { Map, Set } = require('immutable')
+const { combineReducers, createStore } = require('redux')
+const { mapValues } = require('lodash')
+const { Set } = require('immutable')
 
-const bookReducer = (state, action) => {
-  if (state === undefined) {
-    state = Map({books: Set(), customers: Set()})
-  }
+const bookReducer = (state = Set(), action) => {
   if (action.type === 'ADD_BOOK') {
-    return state.set('books', state.get('books').add(action.title))
+    return state.add(action.title)
   }
   if (action.type === 'REMOVE_BOOK') {
-    return state.set('books', state.get('books').delete(action.title))
-  }
-  if (action.type === 'ADD_CUSTOMER') {
-    return state.set('customers', state.get('customers').add(action.customer))
-  }
-  if (action.type === 'REMOVE_CUSTOMER') {
-    return state.set('customers', state.get('customers').delete(action.customer))
+    return state.delete(action.title)
   }
   return state
 }
 
-const store = createStore(bookReducer)
+const customerReducer = (state = Set(), action) => {
+  if (action.type === 'ADD_CUSTOMER') {
+    return state.add(action.customer)
+  }
+  if (action.type === 'REMOVE_CUSTOMER') {
+    return state.delete(action.customer)
+  }
+  return state
+}
 
-store.subscribe(() => {
-  console.log('new state', store.getState())
-})
+const store = createStore(combineReducers({
+  books: bookReducer,
+  customers: customerReducer
+}))
 
-const assertStore = (obj) => {
-  assert.deepStrictEqual(obj, store.getState().toJS())
+const assertStore = obj => {
+  assert.deepStrictEqual(obj, mapValues(store.getState(), value => value.toJS()))
 }
 
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
