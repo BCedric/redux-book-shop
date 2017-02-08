b1d17bb1a043b03d0fdc48cdb6c48735fcc4056a
BCedric <skalimero@gmx.fr>
2017-02-08 09:54:52 +0100

    feat: customers management (#8)
    Fixes #2

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
+const assert = require('assert')
 const { createStore } = require('redux')
-const { Set } = require('immutable')
+const { Map, Set } = require('immutable')
 
-const store = createStore((state, action) => {
+const bookReducer = (state, action) => {
   if (state === undefined) {
-    state = { books: new Set() }
+    state = Map({books: Set(), customers: Set()})
   }
-
   if (action.type === 'ADD_BOOK') {
-    return { books: state.books.add(action.title)}
+    return state.set('books', state.get('books').add(action.title))
   }
-
   if (action.type === 'REMOVE_BOOK') {
-    return { books: state.books.delete(action.title)}
+    return state.set('books', state.get('books').delete(action.title))
   }
-
+  if (action.type === 'ADD_CUSTOMER') {
+    return state.set('customers', state.get('customers').add(action.customer))
+  }
+  if (action.type === 'REMOVE_CUSTOMER') {
+    return state.set('customers', state.get('customers').delete(action.customer))
+  }
   return state
-})
+}
+
+const store = createStore(bookReducer)
 
 store.subscribe(() => {
   console.log('new state', store.getState())
 })
 
+const assertStore = (obj) => {
+  assert.deepStrictEqual(obj, store.getState().toJS())
+}
 
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
+assertStore({ books: ['Война и мир, Voïna i mir'], customers: [] })
+
 store.dispatch({ type: 'ADD_BOOK', title: 'Война и мир, Voïna i mir' })
+assertStore({ books: ['Война и мир, Voïna i mir'], customers: [] })
+
 store.dispatch({ type: 'REMOVE_BOOK', title: 'Война и мир, Voïna i mir' })
+assertStore({ books: [], customers: [] })
+
+store.dispatch({ type: 'ADD_CUSTOMER', customer: 'Tintin' })
+assertStore({ books: [], customers: ['Tintin'] })
+
+store.dispatch({ type: 'REMOVE_CUSTOMER', customer: 'Tintin' })
+assertStore({ books: [], customers: [] })
