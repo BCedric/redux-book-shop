724d21281caae90a4965f9fed49b30c4331d9338
Julien Fontanet <julien.fontanet@isonoe.net>
2017-02-09 11:01:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const assert = require('assert')
 const { combineReducers, createStore } = require('redux')
+const { Map, Set } = require('immutable')
 const { mapValues } = require('lodash')
-const { Set, Map } = require('immutable')
 
 function createReducer (initialState, handlers) {
   return function reducer (state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action)
-    } else {
-      return state
+    if (Object.prototype.hasOwnProperty.call(handlers, action.type)) {
+      return handlers[action.type](state, action.payload)
     }
+    return state
   }
 }
 
 const bookReducer = createReducer(Map(), {
-  ADD_BOOK (state, action) {
-    return state.set(action.isbn, Map({'title': action.title}))
+  ADD_BOOK (state, { isbn, title }) {
+    return state.set(isbn, Map({ title }))
   },
-  REMOVE_BOOK (state, action) {
-    return state.delete(action.isbn)
+  REMOVE_BOOK (state, { isbn ) {
+    return state.delete(isbn)
   }
 })
 
 const customerReducer = createReducer(Set(), {
-  ADD_CUSTOMER (state, action) {
-    return state.add(action.customer)
+  ADD_CUSTOMER (state, { customer }) {
+    return state.add(customer)
   },
-  REMOVE_CUSTOMER (state, action) {
-    return state.delete(action.customer)
+  REMOVE_CUSTOMER (state, { customer }) {
+    return state.delete(customer)
   }
 })
 
@@ -40,46 +39,48 @@ const assertStore = obj => {
   assert.deepStrictEqual(obj, mapValues(store.getState(), value => value.toJS()))
 }
 
-function addBook (title, isbn) {
+function addBook (isbn, title) {
   return {
     type: 'ADD_BOOK',
-    isbn,
-    title
+    payload: {
+      isbn,
+      title
+    }
   }
 }
 
 function removeBook (isbn) {
   return {
     type: 'REMOVE_BOOK',
-    isbn
+    payload: { isbn }
   }
 }
 
 function addCustomer (customer) {
   return {
     type: 'ADD_CUSTOMER',
-    customer
+    payload: { customer }
   }
 }
 
 function removeCustomer (customer) {
   return {
     type: 'REMOVE_CUSTOMER',
-    customer
+    payload: { customer }
   }
 }
 
-store.dispatch(addBook('Война и мир, Voïna i mir', '978-2020476966'))
-assertStore({books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' } }, customers: []})
+store.dispatch(addBook('978-2020476966', 'Война и мир, Voïna i mir'))
+assertStore({ books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' } }, customers: [] })
 
-store.dispatch(addBook('Война и мир, Voïna i mir', '978-2020476966'))
-assertStore({books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' } }, customers: []})
+store.dispatch(addBook('978-2020476966', 'Война и мир, Voïna i mir'))
+assertStore({ books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' } }, customers: [] })
 
 store.dispatch(removeBook('978-2020476966'))
 assertStore({ books: {}, customers: [] })
 
 store.dispatch(addCustomer('Tintin'))
-assertStore({ books: {}, customers: ['Tintin'] })
+assertStore({ books: {}, customers: [ 'Tintin' ] })
 
 store.dispatch(removeCustomer('Tintin'))
 assertStore({ books: {}, customers: [] })
