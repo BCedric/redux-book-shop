da9dfcbf34107d76fbe63c13b4c44daf97e48d46
BCedric <skalimero@gmx.fr>
2017-02-14 11:45:33 +0100

    feat : add user auth

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const assert = require('assert')
-const { combineReducers, createStore } = require('redux')
+const { applyMiddleware, combineReducers, createStore } = require('redux')
 const { createSelector } = require('reselect')
+const fs = require('fs')
 const { Map, Set } = require('immutable')
 const { mapValues } = require('lodash')
+const thunkMiddleware = require('redux-thunk')
+const createLogger = require('redux-logger')
+// const fetch = require('isomorphic-fetch')
 
 // ===================================================================
 
@@ -69,31 +73,76 @@ const customerReducer = combineActionHandlers(Set(), {
   [removeCustomer]: (state, { customer }) => state.delete(customer)
 })
 
-const store = createStore(combineReducers({
-  books: bookReducer,
-  customers: customerReducer
-}))
+const receiveLogin = createActionCreator(
+    'RECEIVE_LOGIN',
+    (user, password, users) => ({user, password, users})
+)
+
+const logout = createActionCreator(
+    'LOGOUT',
+    user => ({ user })
+)
+
+const authReducer = combineActionHandlers('', {
+  [receiveLogin]: (state, action) => action.users.find(u => u.login === action.user && u.password === action.password) !== undefined ? action.user : '',
+  [logout]: (state, action) => null
+})
+
+function logUser (user, password) {
+  return function (dispatch) {
+    return fs.readFile('./users.json', 'utf8', function (err, data) {
+      if (err) {
+        console.log(err)
+        throw err
+      }
+      return data
+    })
+      .then(response => response.json())
+      .then(users =>
+        dispatch(receiveLogin(user, password, users))
+      )
+  }
+}
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  combineReducers(
+    {
+      books: bookReducer,
+      customers: customerReducer,
+      auth: authReducer
+    },
+    applyMiddleware(
+      thunkMiddleware, // lets us dispatch() functions
+      loggerMiddleware // neat middleware that logs actions
+    )
+  )
+)
 
 // ===================================================================
 
 const assertStore = obj => {
-  assert.deepStrictEqual(obj, mapValues(store.getState(), value => value.toJS()))
+  assert.deepStrictEqual(obj, mapValues(store.getState(), value => value instanceof Map || value instanceof Set ? value.toJS() : value))
 }
 
+store.dispatch(logUser('Francis', 'coucou'))
+// store.dispatch(logout())
+
 store.dispatch(addBook('978-2020476966', 'Война и мир, Voïna i mir'))
-assertStore({ books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' } }, customers: [] })
+assertStore({ books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' } }, customers: [], auth: '' })
 
 store.dispatch(addBook('978-2020476967', 'azert'))
-assertStore({ books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' }, '978-2020476967': { title: 'azert' } }, customers: [] })
+assertStore({ books: { '978-2020476966': { title: 'Война и мир, Voïna i mir' }, '978-2020476967': { title: 'azert' } }, customers: [], auth: '' })
 
 assert.deepStrictEqual({ title: 'azert' }, getBookByISBN(store.getState(), '978-2020476967').toJS())
 
 store.dispatch(removeBook('978-2020476967'))
 store.dispatch(removeBook('978-2020476966'))
-assertStore({ books: {}, customers: [] })
+assertStore({ books: {}, customers: [], auth: '' })
 
 store.dispatch(addCustomer('Tintin'))
-assertStore({ books: {}, customers: [ 'Tintin' ] })
+assertStore({ books: {}, customers: [ 'Tintin' ], auth: '' })
 
 store.dispatch(removeCustomer('Tintin'))
-assertStore({ books: {}, customers: [] })
+assertStore({ books: {}, customers: [], auth: '' })
